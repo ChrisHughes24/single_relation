aa6c06db50477f09e9f61b95cd4f644b520ff2de
Chris Hughes <chrishughes24@gmail.com>
2020-09-21 15:39:11 +0100

    docstrings for base case

--- src/base_case.lean
+++ src/base_case.lean
@@ -1,7 +1,15 @@
 import .functor
 import .for_mathlib.coprod.free_group_subgroup
 import .initial
+/-!
 
+# The base case of the `group_thingy` tactic
+
+## Main definitions
+The only definition used outside of this file is `base_case_solver`.
+`base_case_solver T r₁ r₂` solves the word problem for the relation `of' r₁ r₂`, returning
+a normalized word in `T` when possible, and `none` otherwise
+-/
 variables {ι : Type} [decidable_eq ι]
 
 open multiplicative free_group P semidirect_product
@@ -38,7 +46,10 @@ if p.right ∈ closure_var T
   then some p
   else none
 
-def base_case_core (r₁ : ι) (r₂ : C∞) : list (Σ i : ι, C∞) →
+/-- `base_case_core` takes a word `l₁` in the free_group as a `list (Σ i : ι, C∞)`
+and a normalized word with proof `p` as a `P (free_group ι)`.
+It returns a normalized version `reverse l₁ * p`, reduced modulo `of' r₁ r₂`  -/
+@[inline] def base_case_core (r₁ : ι) (r₂ : C∞) : list (Σ i : ι, C∞) →
   P (free_group ι) → P (free_group ι)
 | []     p := p
 | (i::l₁)  ⟨p, ⟨[], _⟩⟩ :=
@@ -63,10 +74,13 @@ def base_case_core (r₁ : ι) (r₂ : C∞) : list (Σ i : ι, C∞) →
         else base_case_core l₁ (inr (of' i.1 i.2) * ⟨p, ⟨j::l₂, sorry⟩⟩)
     else base_case_core l₁ (inr (of' i.1 i.2) * ⟨p, ⟨j::l₂, sorry⟩⟩)
 
-def base_case (r₁ : ι) (r₂ : C∞) (w : free_group ι) : P (free_group ι) :=
+/-- `base_case` reduces a word `w` in the `free_group ι` modulo `of' r₁ r₂` -/
+@[inline] def base_case (r₁ : ι) (r₂ : C∞) (w : free_group ι) : P (free_group ι) :=
 base_case_core r₁ r₂ w.to_list.reverse 1
 
-def base_case_solver (T : set ι) [decidable_pred T] (r₁ : ι) (r₂ : C∞) : solver (of' r₁ r₂) T :=
+/-- `base_case_solver T r₁ r₂` solves the word problem for the relation `of' r₁ r₂`, returning
+a normalized word in `T` when possible, and `none` otherwise -/
+@[inline] def base_case_solver (T : set ι) [decidable_pred T] (r₁ : ι) (r₂ : C∞) : solver (of' r₁ r₂) T :=
 λ w, let p := base_case r₁ r₂ w in
 if p.right ∈ closure_var T
   then some p
