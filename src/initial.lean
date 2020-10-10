938644d894449b5889fe06f57bc5d0d94bcb4489
Chris Hughes <chrishughes24@gmail.com>
2020-10-10 11:52:34 +0100

    Update initial.lean

--- src/initial.lean
+++ src/initial.lean
@@ -8,16 +8,20 @@ for `free_group` and `semidirect_product`
 -/
 notation `C∞` := multiplicative ℤ
 
-variables {ι : Type}
+variables {ι : Type} [decidable_eq ι]
 
 instance : has_repr C∞ := int.has_repr
 
-instance [has_repr ι] [decidable_eq ι] : has_repr (free_group ι) :=
+instance [has_repr ι] : has_repr (free_group ι) :=
 ⟨λ x, repr x.to_list⟩
 
 instance {G H : Type*} [group G] [group H] [has_repr G] [has_repr H] (φ) :
   has_repr (G ⋊[φ] H) := ⟨λ g, "⟨" ++ repr g.1 ++ ", " ++ repr g.2 ++ "⟩" ⟩
 
+/-- `mem_closure_var T w` is true when `w` contains only letters from `T`.  -/
+def mem_closure_var (T : set ι) [decidable_pred T] (w : free_group ι) : bool :=
+w.to_list.all (λ i, i.1 ∈ T)
+
 /-- `of_list` is an unsafe function turning a ` list (Σ i : ι, C∞)` into
   an element of the `free_group`, ignoring the proof that the list is reduced. -/
-meta def of_list (l : list (Σ i : ι, C∞)) : free_group ι := ⟨l, undefined⟩
+meta def of_list {ι : Type} (l : list (Σ i : ι, C∞)) : free_group ι := ⟨l, undefined⟩
