1dffa8e804edae797875e0232b3ef0fdfa9a6850
Chris Hughes <chrishughes24@gmail.com>
2020-10-10 12:44:18 +0100

    add solve_by_trivial

---
+++ src/solve_by_trivial.lean
@@ -0,0 +1,17 @@
+import P
+/-!
+# Solve by trivial
+This file contains one definition, `solve_by_trivial`.
+
+`solve_by_trivial` solves the word problem in the easy case when
+every letter in `w` is already in `T`.
+-/
+
+open semidirect_product
+
+def solve_by_trivial  {ι : Type} [decidable_eq ι] (T : set ι)
+  [decidable_pred T] (w : free_group ι) (vars_w : list ι) :
+  option (P (free_group ι)) :=
+if vars_w.all (∈ T)
+  then some (inr w)
+  else none
